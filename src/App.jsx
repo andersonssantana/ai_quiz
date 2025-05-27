20cb99165f2cd03611f6a053ab2686481446d494
Anderson Santana <anderslash@gmail.com>
2025-05-27 14:03:33 -0300

    refactor App.jsx to remove unused useEffect and update prompt generation to use dynamic subject input

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import { GoogleGenerativeAI } from '@google/generative-ai';
 import './App.css'
 
@@ -11,25 +11,22 @@ const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
 function App() {
   const [res, setRes] = useState();
-
-  useEffect(() => {
+  const [subject, setSubject] = useState('qualquer tema')
 
   const generateContent = async () => {
-    const prompt = `Gere 5 perguntas de múltipla escolha sobre games online. Produza um JSON no formato a seguir:
+    const prompt = `Gere 5 perguntas de múltipla escolha sobre ${subject}. Produza um JSON no formato a seguir:
     
     Questions = { "question": string, "options": array<string>, "correct": string }
     Return: array<Questions>`
       const result = await model.generateContent(prompt);
       const cleanedResult = result.response.text().replace(/```json|```|\n`$/g, '').trim()
-      console.log(cleanedResult);
       setRes(JSON.parse(cleanedResult));
   }
-  generateContent();
-  },[])
 
   return (
     <div>
-     {JSON.stringify(res)}
+    <input onChange={ e => setSubject(e.target.value) }></input>
+    <button onClick={ generateContent }>Gerar quiz</button>
     </div>
   )
 }
